d9bd01d4e98acc30f7696d98d8346d87a01f94d0
Cassiano Cruz <cassiano.cruz@pop-pr.rnp.br>
2018-07-20 15:57:06 -0300

    Change lock location

--- director/scripts/entrypoint.sh
+++ director/scripts/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ENTRYPOINT_D="/docker-entrypoint.d/"
+
 LOCK="entrypoint.lock"
 SRC_DIR="/usr/src/bacula"
 BACULA_DIR="/etc/bacula"
 
-if [ -f "$ENTRYPOINT_D$LOCK" ]; then
+if [ -f "$BACULA_DIR$LOCK" ]; then
     echo "Entrypoint Script already runned"
     exit 0
 fi
@@ -13,7 +13,7 @@ fi
 if [ -d $SRC_DIR ]; then
     cp -rf $SRC_DIR $BACULA_DIR
     if [ -t $? ]; then
-        touch $ENTRYPOINT_D$LOCK
+        touch $BACULA_DIR$LOCK
     fi
     echo "Entrypoint Finished"
 fi
