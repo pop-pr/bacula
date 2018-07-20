88cc8e1aab2a380a40eadf7a50d2b5e697bdffab
Cassiano Cruz <cassiano.cruz@pop-pr.rnp.br>
2018-07-20 17:18:32 -0300

    addin start_process function

--- director/scripts/entrypoint.sh
+++ director/scripts/entrypoint.sh
@@ -5,15 +5,31 @@ LOCK="entrypoint.lock"
 SRC_DIR="/usr/src/bacula"
 BACULA_DIR="/etc/bacula"
 
-if [ -f "$BACULA_DIR$LOCK" ]; then
+if [ -f "$BACULA_DIR/$LOCK" ]; then
     echo "Entrypoint Script already runned"
+    start_process    
     exit 0
 fi
 
 if [ -d $SRC_DIR ]; then
     cp -rf $SRC_DIR $BACULA_DIR
     if [ -t $? ]; then
-        touch $BACULA_DIR$LOCK
-    fi
-    echo "Entrypoint Finished"
+        echo "creating lock_file"
+        touch "$BACULA_DIR/$LOCK"
+        echo "$BACULA_DIR/$LOCK"
+    fi    
+    start_process
 fi
+
+start_process()
+{
+    echo "Starting processes"
+    /etc/init.d/bacula-director start 
+    /etc/init.d/bacula-fd start 
+    /etc/init.d/bacula-sd start 
+    /etc/init.d/ssh start 
+    /etc/init.d/apache2 start 
+    bconsole
+}
+echo "Entrypoint Finished"
+exit 0
