b9cc68795ddc3800dfac0eaf90918bfc2a614585
Cassiano Cruz <cassiano.cruz@pop-pr.rnp.br>
2018-07-24 16:57:29 -0300

    Script reformulado, novos métodos, mais modularização

--- director/scripts/entrypoint.sh
+++ director/scripts/entrypoint.sh
@@ -4,43 +4,91 @@ LOCK="entrypoint.lock"
 SRC_DIR="/usr/src/bacula"
 BACULA_DIR="/etc/bacula"
 CP="/bin/cp -Rf"
+CHWON="/bin/chown -R"
 
 start_process()
 {
-    echo "Starting processes"
-    /etc/init.d/bacula-director start 
+    echo "Starting processes..."
     /etc/init.d/bacula-fd start 
     /etc/init.d/bacula-sd start 
+    /etc/init.d/bacula-director start     
     /etc/init.d/ssh start 
-    /etc/init.d/apache2 start 
-    bconsole
+    /etc/init.d/apache2 start    
+    echo "Done!"
+}
+
+test_bacula()
+{
+    local result=1
+    local retval=1
+    echo 'reload' | bconsole && result=0
+    if [ $result -eq 0 ]; then
+        retval=0    
+    fi
+    return $retval
 }
 
 test_lock()
 {
+    local retval=1
     if [ -f "$BACULA_DIR/$LOCK" ]; then
         echo "Entrypoint Script already runned"
-        start_process    
-        exit 0
-    fi  
+        retval=0
+    fi
+    return $retval   
+}
+
+create_lock()
+{
+    # $1 == result from copy_file function
+    local result=1
+    if [ $1 -eq 0 ]; then              
+        echo "Creating lock file.."
+        touch "$BACULA_DIR/$LOCK" && result=0
+        if [ $result -eq 0 ]; then
+            echo "Done!"
+        else
+            echo "Something went wrong while creating lock file"
+            exit 1
+        fi        
+    fi    
+}
+
+change_perms()
+{
+    $CHWON bacula:bacula $BACULA_DIR
+}
+
+copy_files()
+{
+    local result=1
+    if [ -d $SRC_DIR ]; then                
+        $CP $SRC_DIR/* $BACULA_DIR/ && result=0
+    fi    
+    return $result 
 }
 
 main()
 {
     test_lock
-    if [ -d $SRC_DIR ]; then        
-        result=0
-        $CP $SRC_DIR/* $BACULA_DIR/ && result=1
-        if [ $result -eq 1 ]; then
-            echo "creating lock_file"
-            touch "$BACULA_DIR/$LOCK"
-            echo "$BACULA_DIR/$LOCK"
-        fi    
+    local lock=$?    
+    if [ $lock -eq 0 ]; then
         start_process
+        test_bacula    
+        exit 0
     fi
-    echo "Entrypoint Finished"
+    copy_files    
+    local result=$?    
+    create_lock $result            
+    change_perms
+    start_process
+    test_bacula
+    local test=$?    
+    if [ $test -eq 1 ]; then        
+        echo 'Failed'
+        exit 1
+    fi
+    echo "Entrypoint Finished"    
     exit 0
 }
 main
-
-
