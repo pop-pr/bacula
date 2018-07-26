8d5f4d8b3436cc50b98a19725de37442c160abbc
Cassiano Cruz <cassiano.cruz@pop-pr.rnp.br>
2018-07-26 17:40:27 -0300

    Trocado de wget para tar

--- director/scripts/installWebBacula.sh
+++ director/scripts/installWebBacula.sh
@@ -18,24 +18,18 @@ fi
 cd "$BACULA_SCRIPT_DIR"
 
 if [ ! -d $WEBACULA_GZ ] && [ ! -d "webacula" ]; then
-    echo "Downloading WeBacula"
-    result = $(wget --no-check-certificate -c https://github.com/wanderleihuttel/webacula/archive/7.5.3.tar.gz)
-    if [ -t $result ]; then
-        tar xvzf 7.5.3.tar.gz
-        mv webacula-7.5.3 $WEBACULA_DIR
-        chown -R www-data.www-data $WEBACULA_DIR
-        chmod 775 "/etc/bacula"
-        chmod 7777 "$WEBACULA_DIR/data/cache"
-        cp "$TEMPLATES/php.ini" /etc/php/7.0/apache2/
-        cp "$TEMPLATES/config.ini" "$WEBACULA_DIR/application/"
-        cp "$TEMPLATES/webacula.conf" $APACHE_SITES
-        a2enmod rewrite
-        cd $APACHE_SITES && a2ensite webacula.conf
-        echo 'www-data ALL=NOPASSWD:/usr/sbin/bconsole' > /etc/sudoers.d/webacula        
-        mv "$BACULA_SCRIPT_DIR$WEBACULA_DIR" $WEB_DIR
-        service apache2 restart
-        echo "Done"
-    else
-        echo "Something went wrong"
-    fi
+    tar xvzf $TEMPLATES/7.5.3.tar.gz
+    mv webacula-7.5.3 $WEBACULA_DIR
+    chown -R www-data.www-data $WEBACULA_DIR
+    chmod 775 "/etc/bacula"
+    chmod 7777 "$WEBACULA_DIR/data/cache"
+    cp "$TEMPLATES/php.ini" /etc/php/7.0/apache2/
+    cp "$TEMPLATES/config.ini" "$WEBACULA_DIR/application/"
+    cp "$TEMPLATES/webacula.conf" $APACHE_SITES
+    a2enmod rewrite
+    cd $APACHE_SITES && a2ensite webacula.conf
+    adduser www-data bacula    
+    mv "$BACULA_SCRIPT_DIR$WEBACULA_DIR" $WEB_DIR
+    service apache2 restart
+    echo "Done"
 fi
