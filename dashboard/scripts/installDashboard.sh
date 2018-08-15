5e03561f5b66f805d4cbc3586d6a974d57ffef3f
Cassiano Cruz <cassiano.cruz@pop-pr.rnp.br>
2018-08-15 16:09:22 -0300

    Correções

--- dashboard/scripts/installDashboard.sh
+++ dashboard/scripts/installDashboard.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Installs WEBACULA WEB SERVICE
+# Installs BACULA WEB DASHBOARD SERVICE
 
-DASHBOARD_DIR="bdashborad"
+DASHBOARD_DIR="bdashboard"
 WEB_DIR="/var/www/html/"
 BACULA_SCRIPT_DIR="/usr/src/bacula/scripts/"
 APACHE_SITES='/etc/apache2/sites-available/'
@@ -10,7 +10,7 @@ DASHBOARD_GZ="dashboard.gz"
 TEMPLATES="dashboard_files"
 
 
-if [ -d $WEB_DIR$WEBACULA_DIR ]; then
+if [ -d $WEB_DIR$DASHBOARD_DIR ]; then
     echo "Dashboard already installed"
     exit 0
 fi
@@ -18,17 +18,12 @@ fi
 cd "$BACULA_SCRIPT_DIR"
 
 if [ ! -d "bdashboard" ]; then
-    tar xvzf $TEMPLATES/$DASHBOARD_GZ
-    # mv webacula-7.5.3 $DASHBOARD_DIR
+    tar xvzf $TEMPLATES/$DASHBOARD_GZ    
     chown -R www-data.www-data $DASHBOARD_DIR
-    chmod 775 "/etc/bacula"
-    chmod 7777 "$DASHBOARD_DIR/data/cache"
-    # cp "$TEMPLATES/php.ini" /etc/php/7.0/apache2/
-    cp "$TEMPLATES/config.php" "$DASHBOARD_DIR/application/config/"
-    # cp "$TEMPLATES/webacula.conf" $APACHE_SITES
-    a2enmod rewrite
-    cd $APACHE_SITES && a2ensite webacula.conf
-    adduser www-data bacula    
+    chmod 775 "/etc/bacula"        
+    cp "$TEMPLATES/config.php" "$DASHBOARD_DIR/application/config/"    
+    a2enmod rewrite  
+    adduser www-data bacula
     mv "$BACULA_SCRIPT_DIR$DASHBOARD_DIR" $WEB_DIR
     service apache2 restart
     echo "Done"
